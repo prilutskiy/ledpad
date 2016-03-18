94d6d90bd8f9ac026c781909c2b59b2ab72cbe4e
Max Prilutskiy <maks.prilutskiy@gmail.com>
2016-03-18 21:34:27 +0300

    ui stub added

--- manager/main.js
+++ manager/main.js
@@ -1,1 +1,20 @@
-console.log("Hello!")
+var express = require('express');
+var app = express();
+var path = require('path');
+
+function entryPoint(req, res) {
+  console.log('Requested page: ' + req.params.page);
+  res.sendFile(path.resolve(__dirname + '/../pages/index.html'));
+}
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname + '/ui/index.html'));
+});
+
+app.use("/pages", express.static(__dirname + '/ui/pages'));
+app.use("/assets", express.static(__dirname + '/ui/assets'));
+app.use("/dist", express.static(__dirname + '/ui/dist'));
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
